9afa1c9644fb4149e8202968db1bdb88d9baf380
Eduardo Matheus Oliveira de Córdova <eduardocordova21@outlook.com.br>
2021-11-18 00:37:05 -0300

    Adicionar arquivos de projeto.

---
+++ ScrumWayy/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using ScrumWayy.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace ScrumWayy.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ ScrumWayy/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using ScrumWayy.Models;
+using ScrumWayy.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ScrumWayy.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
